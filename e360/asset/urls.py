0a71fc9869b8a78f28d49d913d1d2db4ea1699ae
Paul Williams <paul.d.williamsxiv@gmail.com>
2023-11-13 16:42:05 -0500

    added loan detail

--- e360/asset/urls.py
+++ e360/asset/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # Equipment
     path('equipment-dash/',views.equipment_dash,name='equipment dash'),
     path('equipment-update/<str:pk>',UpdateEquipment.as_view(),name='update equipment'),
+    
     # Vehicles
     path('vehicle-dash/',views.vehicle_dash,name='vehicle dash'),
     path('vehicle-update/<str:pk>',UpdateVehicle.as_view(),name='update vehicle'),
